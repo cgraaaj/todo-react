8ae67fabb9c6f1d5122f3c35fe42198d1eeac599
Rajvel Govindarajan <cgrvel@gmail.com>
2022-05-11 20:11:47 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import "./styles.css";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [state, setState] = useState("");
+  const [todo, setTodo] = useState([]);
+  useEffect(() => {}, [state]);
+
+  function onFormSubmit(e) {
+    e.preventDefault();
+    console.log(state);
+    let t = todo;
+    t.push(state);
+    setTodo([...t]);
+    console.log(todo);
+  }
+
+  function onInputChnage(e) {
+    setState(e.target.value);
+  }
+
+  function renderList() {
+    return todo.map((data) => <li key={data}>{data}</li>);
+  }
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <form onSubmit={onFormSubmit}>
+        <input onChange={onInputChnage}></input>
+      </form>
+      {renderList()}
     </div>
   );
 }
